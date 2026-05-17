14fe3e6ce4d03da8d833b582164298ab2eac4e1a
careroms <wasu.github@gmail.com>
2026-05-17 18:02:36 +0700

    remove unused file

--- program/processor/base_executor.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any, Optional
-
-
-class BaseExecutor(ABC):
-  @abstractmethod
-  def execute(self) -> Any:
-    """Execute this executor's duty and return a typed result."""
-    pass
